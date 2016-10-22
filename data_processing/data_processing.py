f71d00a848e6835afbedb4cd0784b00a31653654
crivera5 <christopher_rivera@intuit.com>
2016-10-22 16:15:33 -0700

    added tranformations.py with scale() and viz.py for plotting

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -1,5 +1,8 @@
 # Functions for handling data processing
 
+from os import listdir
+from os.path import join, split
+
 import pandas as pd
 from scipy.io import loadmat
 
@@ -38,3 +41,101 @@ def load_data(path, convert_index=True):
         sequence = int(data['sequence'])
 
     return df, sampling_rate, sequence
+
+
+def extract_base_stats(data, patient=1, number=1, condition=0):
+    """extracts the mean, variance, skew and kurtosis of a given data set of data set
+    data(dataframe): a given data set
+    patient(int): the patient
+    number(int): the trace number
+    conditon(int): 0 if interictal or 1 if ictal
+    """
+    mean = data.mean()
+    var = data.var()
+    skew = data.skew()
+    kurtosis = data.kurtosis()
+
+    l = len(mean)
+
+    # format the namea
+
+    def create_tuple(name):
+        return [(name, str(i + 1)) for i in xrange(l)]
+
+    columns = create_tuple('mean') + create_tuple('variance') + create_tuple('skew') + create_tuple('kurtosis')
+
+    res = pd.DataFrame(pd.concat([mean, var, skew, kurtosis])).T
+    res.columns = pd.MultiIndex.from_tuples(columns, names=['Statistic', 'Channel'])
+    res.index = pd.MultiIndex.from_tuples([(patient, number, condition)], names=['Patient', 'TraceNumber', 'Condition'])
+    return res
+
+
+def parse_filename(filename):
+    """Parses m filename to get the pertinent information"""
+
+    # strip out the .mat
+    filename = filename.replace('.mat', '')
+
+    # parse the remaing part
+    return [int(part) for part in filename.split('_')]
+
+
+def count_files(directory):
+    """counts the number of files per condition for each patient in a directory"""
+
+    # parse the files
+    files = [parse_filename(f) for f in listdir(directory) if f.endswith('.mat')]
+
+    # go through each file and count
+    interictal = {}
+    postictal = {}
+
+    for f in files:
+        if f[2] == 0:
+            interictal.setdefault(f[0], 0)
+            interictal[f[0]] += 1
+        else:
+            postictal.setdefault(f[0], 0)
+            postictal[f[0]] += 1
+    return interictal, postictal
+
+
+def get_files():
+    """gets the file names"""
+    base = '/Users/crivera5/Documents/NonIntuitProjects/Kaggle/KaggleEEG'
+    path1 = join(base, 'train_1')
+    path2 = join(base, 'train_2')
+    path3 = join(base, 'train_3')
+
+    def get_fs(path):
+        return [join(path, f) for f in listdir(path) if f.endswith('.mat')]
+
+    return get_fs(path1) + get_fs(path2) + get_fs(path3)
+
+
+def get_stats_from_one_file(filename):
+    """gets the stats from one file
+    Parameters:
+        filename(str): absolute path to the file
+    Return(df): 1 row data frame with the stats
+
+    """
+    try:
+        # get the data
+        data = load_data(filename)[0]
+        # get the file name parts
+        patient, number, condition = parse_filename(split(filename)[1])
+
+        # get the stats and return
+        return extract_base_stats(data, patient, number, condition)
+    except ValueError:
+        return None
+
+
+def get_stats():
+    """function to get the stats and put into a data frame """
+    # get the files names
+    files = get_files()
+
+    # use a slow for loop to get the res
+    return pd.concat(map(get_stats_from_one_file, files))
