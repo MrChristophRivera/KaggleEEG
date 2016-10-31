6dd93affb87519cb19198384b4c9065f17baad74
crivera5 <christopher_rivera@intuit.com>
2016-10-30 22:43:26 -0700

    added psi and get_psi to feature extraction.py

--- data_processing/feature_extraction.py
+++ data_processing/feature_extraction.py
@@ -2,9 +2,11 @@ import numpy as np
 import numpy.linalg as la
 import pandas as pd
 
+from transformations import transform_psd
+
 
 def extract_correlations(data):
-    """Calculate the correlation matrix for time series with its associated eigenvalues and the eigenvalues
+    """Calculate a correlation matrix with its associated eigenvalues and the eigenvalues
     Parameters:
         data(pd.DataFrame): Data frame with the time series data
     Returns:
@@ -35,3 +37,58 @@ def extract_correlations(data):
     eigs.index = map(lambda x: 'eig_%d' % (x + 1), list(eigs.index))
 
     return pd.DataFrame(pd.concat([corr, eigs])).T
+
+
+########################################################################################################################
+# FFT transform related
+########################################################################################################################
+
+
+def psi(psd_series, index=0):
+    """Gets thePower Spectral Intensity of the standard bands for one time series
+    This is using the standard bands of:
+        δ(0.5–4Hz), θ(4–7Hz), α(8–12Hz), β(12–30Hz), and γ(30–100Hz)
+    Parameters:
+        psd_df(pd.DataFrame): a data frame with index frequencies and power.
+        index(int): an index for labeling
+    Returns:
+        bands_df(pd.DataFrame): a data frame with the band sums
+    """
+
+    # compute the sums
+
+    bands_df = pd.DataFrame({'channel %d δ(0.5–4Hz)' % (index + 1): psd_series[psd_series.between(0, 0.5)].sum(),
+                             'channel %d θ(4–7Hz) ' % (index + 1): psd_series[psd_series.between(4, 7)].sum(),
+                             'channel %d α(8–12Hz)' % (index + 1): psd_series[psd_series.between(8, 12)].sum(),
+                             'channel %d  β(12–30Hz)' % (index + 1): psd_series[psd_series.between(12, 30)].sum(),
+                             'channel %d γ(30–100Hz)' % (index + 1): psd_series[psd_series.between(30, 100)].sum()},
+                            index=[1])
+    return bands_df
+
+
+def get_psi(psd_df):
+    """Gets thePower Spectral Intensity of the standard bands for all time series.
+    This is using the standard bands of:
+        δ(0.5–4Hz), θ(4–7Hz), α(8–12Hz), β(12–30Hz), and γ(30–100Hz)
+    Parameters:
+        psd_df(pd.DataFrame): a data frame with index frequencies and power.
+    Returns:
+        psi_df(pd.DataFrame): a data frame with the band sums
+    """
+    # do the calcs
+    psi_df = [psi(psd_df.iloc[:, i], i) for i in range(16)]
+    return pd.concat(psi_df, axis=1)
+
+
+class FFT_Features(object):
+    """ Class for extracting multiple features from a fft
+        These features include:
+            1. PowerSpectral Intensity
+            2. Relative Intensity.
+            3. Correlation of the PSD
+            4. eigen values of the psd
+    """
+
+    def __init__(self, data):
+        # transform the psd
+        self.psd = transform_psd(data)
