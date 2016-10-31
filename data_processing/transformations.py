e02bafe03efd956c977835c0eb50ca620e41b9ee
crivera5 <christopher_rivera@intuit.com>
2016-10-30 17:21:00 -0700

    added extract correlation

--- data_processing/transformations.py
+++ data_processing/transformations.py
@@ -10,3 +10,24 @@ def scale(x):
     Returns:
         x_scaled(pd.Series)"""
     return (x - np.mean(x)) / np.std(x)
+
+
+def extract_correlation(df):
+    """Calculate the correlation matrix for time series with its associated eigenvalues and the eigenvalues
+    Parameters:
+        df(pd.DataFrame): Data frame with the time series data
+    Returns:
+        corr(pd.Series): The correlations the time series
+        eigs(pd.Series): The eigenvalues.
+
+    """
+
+    # calcualte the correlation matrix, convert the lower left triangle into a np.nan.
+    corr = data.corr()
+    # calculate the eigenvalues
+    eigs = pd.Series(LA.eigvals(corr))
+    corr.values[np.tril_indices_from(corr)] = np.nan
+
+    # Unstack, return the values that are not null and return
+    corr = corr.unstack()
+    return corr[corr.isnull() == False], eigs
