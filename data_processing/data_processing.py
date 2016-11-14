232c18e28e8be4a207a3fbb543ccee5ceede8623
Anthony Le <Anthony Le>
2016-11-13 17:06:58 -0800

    REF: Added processor class.

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -142,3 +142,61 @@ def get_stats():
     return pd.concat(map(get_stats_from_one_file, files))
 
 
+class Processor(object):
+
+    def __init__(self, list_of_functions, dtrend=None):
+        self.list_of_functions = list_of_functions
+        self.dtrend = dtrend
+
+    def process_folder(train_path, function_name):
+        """ Apply function to all files in
+        """
+        seizure_df = pd.DataFrame()
+        failures = []
+        results = []
+        print(train_path)
+        for patient_path in train_path:
+            # This is how I speed up processing 4x by making full use of all cores in the CPUs.
+            values = [delayed(function_name)('\\'.join([patient_path, f])) for f in listdir(patient_path) if isfile('/'.join([patient_path, f]))]
+            result = compute(*values, get=dask.multiprocessing.get)
+            results.append(result)
+        return results
+
+    def process_file(self, fname):
+        """ Apply list of functions to file.
+
+        Each function should return a dataframe with x columns and 1 row. The number of columns is equal to features
+        extracted.
+
+        :param fname:
+        :param list_of_functions:
+        :param dtrend (string):  Must be  'None', 'mean', 'median'
+        :return:
+        """
+        base_path, target_path, f = fname.split('\\')
+        path = '\\'.join([base_path, target_path])
+        print('processing', path, f)
+        df, sampling_rate, sequence = load_data(join(path, f))
+        df = normalize(df, dtrend)
+        # Determine if this is an inter or preictal dataset and put in corresponding bucket.
+        split_string = f.replace('.', '_').split('_')
+        feature_df_list = []
+        for func in self.list_of_functions:
+            # Process function and append index columns
+            func_result_df = func(df)
+            feature_df_list.append(func_result_df)
+        feature_df = pd.concat(feature_df_list, 1)
+        feature_df = append_index(feature_df, split_string)
+        return feature_df
+
+    def append_index(df, split_string):
+        """ Append data set identifier and set index to identifier"""
+        df['patient'] = split_string[0]
+        df['dataset_id'] = split_string[1]
+        df['pre_ictal'] = split_string[2]
+        return df
+
+    def normalize(self):
+        """ Normalize data frame.
+        """
+        pass
