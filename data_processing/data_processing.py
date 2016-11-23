d5f0707e2f460ca9335ddf7471cad305610fd208
crivera5 <christopher_rivera@intuit.com>
2016-11-22 22:52:50 -0800

    Fixed drop_nd_rows to ensure drops the rows, was replacing with zeros.

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -200,7 +200,7 @@ class Processor(object):
         # seizure_df = pd.DataFrame()
         # failures = []
         results = compute(*values, get=dask.multiprocessing.get)
-        results = [res for res in results if res]
+        results = [res for res in results if res is not None]
         return pd.concat(results)
 
     def process_file(self, fname):
@@ -255,7 +255,7 @@ class Processor(object):
 
             # fill all na values to ensure removed.
             df.fillna(method='ffill', inplace=True)
-            df.fillna(method='bill', inplace=True)
+            df.fillna(method='bfill', inplace=True)
         return df
 
 
